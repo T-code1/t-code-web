46a898bd4810a21cc5e1a2d89bd6c44c3f170e8d
UmurerwaDivine <divinekagame@gmail.com>
2020-03-13 08:44:28 +0200

    created contact and about components with unofficial styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import Sample from "./components/sample";
+import About from "./components/About"
+import Contact from './components/Contact';
 class App extends Component {
-  render() { 
-    return ( 
+  render() {
+    return (
       <div className="app">
-       {/* THIS WHERE WE ARE GOING TO CALL OUR COMPONENTS */}
-       <Sample/>
+        {/* THIS WHERE WE ARE GOING TO CALL OUR COMPONENTS */}
+        <Sample />
       </div>
-     );
+    );
   }
 }
- 
+
 export default App;
