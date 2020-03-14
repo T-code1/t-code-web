0d0d615091876835be140552121b0c3336dc61b9
zachee <zacheeishimwe@gmail.com>
2020-03-14 17:36:46 +0200

    call component in the app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import Sample from "./components/sample";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Services from "./components/Services";
+import About from "./components/About";
+import Contact from "./components/Contact";
 class App extends Component {
   render() { 
     return ( 
       <div className="app">
-       {/* THIS WHERE WE ARE GOING TO CALL OUR COMPONENTS */}
+       {/* list of components */}
        <Sample/>
+       <Navbar/>
+       <Home/>
+       <Services />
+       <About/>
+       <Contact/>
       </div>
      );
   }
