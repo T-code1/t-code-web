77d45614cd7385466f98fa0ea044b2ab9ee90f7f
zachee <zacheeishimwe@gmail.com>
2020-04-01 20:26:55 +0200

    use the correct measurement

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import "./styles/home.css";
 class Home extends Component {
     render(){
-        const imageUrl = "https://assets.entrepreneur.com/content/3x2/2000/20150708172005-coding-working-workspace-apple-macintosh.jpeg"
+        // const imageUrl = "https://assets.entrepreneur.com/content/3x2/2000/20150708172005-coding-working-workspace-apple-macintosh.jpeg";
         return (
             <div className = "home">
-            <img className="home-img" src= {imageUrl} alt= "coding" />
+            <img className="home-img" src={require("../images/mac.jpeg")} alt= "coding" />
             <h1 className='text'>Innovation Through Strategy </h1>
             <h1 className='text2'>Design & Technology </h1>
             </div>
