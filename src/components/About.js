f16609df6c0d3283df4f7b828ee2e217342ebc66
UmurerwaDivine <divinekagame@gmail.com>
2020-04-06 15:15:09 +0200

    newFont and pictures

--- src/components/About.js
+++ src/components/About.js
@@ -4,29 +4,29 @@ class About extends Component {
     render() {
         return (
             <div className="container">
-             <div className="contain">
-                <div className="about">
-                    <h2>About Us</h2>
-                    <h3>Our everyday business</h3>
-                </div>
-                <div className="img">
-
-                    <img className="image" src={require("../images/pexels-photo-248159.jpeg")} alt="pexels" />
-
-                    <div className="paragraph">
-
-                        <p>In a world of technology, People make difference</p>
-                        <p>so you've got questions on</p>
-                        <p>which app to use we've got answers:</p>
-                        <p> <i className="fa fa-hand-o-right" />  Website</p>
-                        <p> <i className="fa fa-hand-o-right" />  Mobile Application</p>
-                        <p> <i className="fa fa-hand-o-right" />  Desktop Application</p>
-
+                <div className="contain">
+                    <div className="about">
+                        <h2>About Us</h2>
+                        <h3>Our everyday business</h3>
                     </div>
+                    <div className="img">
+
+                        <img className="image" src={require("../images/placeholder.png")} alt="pexels" />
+
+                        <div className="paragraph">
+
+                            <p>In a world of technology, People make difference</p>
+                            <p>so you've got questions on</p>
+                            <p>which app to use we've got answers:</p>
+                            <p> <i className="fa fa-hand-o-right" />  Website</p>
+                            <p> <i className="fa fa-hand-o-right" />  Mobile Application</p>
+                            <p> <i className="fa fa-hand-o-right" />  Desktop Application</p>
+
+                        </div>
                     </div>
                 </div>
-                </div>
-            
+            </div>
+
         )
     }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ class Home extends Component {
         // const imageUrl = "https://assets.entrepreneur.com/content/3x2/2000/20150708172005-coding-working-workspace-apple-macintosh.jpeg";
         return (
             <div className="home">
-                <img className="home-img" src={require("../images/mac.jpeg")} alt="coding" />
+                <img className="home-img" src={require("../images/3847de7b-b22d-4fe9-8bd6-b4ae846d78df.JPG")} alt="coding" />
                 <h1 className='text'>Innovation Through Strategy </h1>
                 <h1 className='text2'>Design & Technology </h1>
                 <Services />
