10988638fa5e8ef027b4f4e5d340581ebb976386
zachee <zacheeishimwe@gmail.com>
2020-04-21 14:07:51 +0200

    add tools

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,6 +11,18 @@ class Home extends Component {
                 <img className="home-img" src={require("../images/3847de7b-b22d-4fe9-8bd6-b4ae846d78df.JPG")} alt="coding" />
                 <h1 className='text'>Innovation Through  </h1>
                 <h1 className='text2'>Strategy,Design & Technology </h1>
+               <div className="tools">
+               <img className="adobe" src={require("../images/logo-xd.png")} alt="coding" />
+                <img className="figma" src={require("../images/logo-figma.png")} alt="coding" />
+               <img className="js" src={require("../images/js.png")} alt="coding" />
+               <img className="react" src={require("../images/logo-react.png")} alt="coding" />
+               <img className="css" src={require("../images/css.jpg")} alt="coding" />
+               <img className="html" src={require("../images/htmlcss.png")} alt="coding" />
+               <img className="node" src={require("../images/node.png")} alt="coding" />
+                <img className="mongodb" src={require("../images/mongodb.png")} alt="coding" />
+
+
+               </div>
                 <Services />
                 <About />
                 <Contact />
